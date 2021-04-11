7479a1f2a22e8129ddd5a757d8c12d06fb5d6ffc
Antoine KHOW <anto.kh@hotmail.fr>
2021-04-11 18:39:03 +0200

    Changement de la structure de données du Diffuseur et ajout des intéraction RUOK et IMOK avec le Gestionnaire

--- EcouteUtilisateur.java
+++ EcouteUtilisateur.java
@@ -21,11 +21,11 @@ public class EcouteUtilisateur implements Runnable{
                     posMsg = this.liveStream.getListMsg().size() - 1;
                 }
                 String [] ancienMsg = this.liveStream.getListMsg().get(posMsg).split(" ");
-                pw.print(Diffuseur.OLDM + ancienMsg[1] + ancienMsg[2] + ancienMsg[3] + "\n");
+                pw.print(Diffuseur.OLDM + ancienMsg[1] + ancienMsg[2] + ancienMsg[3] + "\r\n");
                 pw.flush();
                 posMsg--;
             }
-            pw.print(Diffuseur.ENDM + "\n");
+            pw.print(Diffuseur.ENDM + "\r\n");
             pw.flush();
         } catch(NumberFormatException e) {
             pw.print("[Erreur] : Format de nombre invalide\n");
@@ -47,13 +47,16 @@ public class EcouteUtilisateur implements Runnable{
                     && traitement[2].length() <= 140) {
                     this.liveStream.getListMsg().add(traitement[2]);
 
-                    pw.print(Diffuseur.ACKM + "\n");
+                    pw.print(Diffuseur.ACKM + "\r\n");
                     pw.flush();
 
                     this.socket.close();
                     break;
                 } else if (traitement[0].equals(Diffuseur.LAST)) {
                     receptionLast(pw, traitement);
+                } else if (traitement[0].equals(Diffuseur.RUOK)) {
+                    pw.print(Diffuseur.IMOK + "\r\n");
+                    pw.flush();
                 } else {
                     pw.print("[Erreur] : Message au mauvais format\n");
                     pw.flush();
